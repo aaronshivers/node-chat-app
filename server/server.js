8d278e77ff6e11338680c3ff58e2cfdbbc07ad55
Aaron Shivers <aaron@moonmail.org>
2019-01-29 17:14:25 -0600

    Added connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 require('dotenv').config()
 
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
-const app = express()
 const { PORT } = process.env
+const app = express()
+const server = http.createServer((app))
+const io = socketIO(server)
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.render('index'))
+// app.get('/', (req, res) => res.render('index'))
 
-app.listen(PORT, () => console.log(`Server running on port: ${ PORT }.`))
+io.on('connection', (socket) => {
+  console.log('New user connected.')
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+  })
+})
+
+server.listen(PORT, () => console.log(`Server running on port: ${ PORT }.`))
